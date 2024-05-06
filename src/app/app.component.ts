75db70d2db50961c37b21c9b6b9da3a9d08f19bb
camilonull <camilo.vargas10@uptc.edu.co>
2024-05-06 16:36:33 -0500

    Add Alert Low Stock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { AuthenticationService } from './service/authentication.service';
 import { AlertService } from './service/alert.service';
+import { ProductService } from './service/product.service';
+import { ProductModel } from './model/ProductModel';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +16,13 @@ export class AppComponent implements OnInit{
   mesage = '';
   shakeAlert: boolean = false;
   navbarVisible: boolean = true;
+  lowStockProducts: ProductModel[] = [];
 
-  constructor(private authService: AuthenticationService, private alertService: AlertService) {}
+  constructor(private authService: AuthenticationService, private alertService: AlertService, private productService: ProductService, private renderer: Renderer2) {
+    this.getProductLowStock();
+  }
+
+
 
   get userOn(): boolean {
     return this.authService.usuarioAutenticado;
@@ -45,6 +52,40 @@ export class AppComponent implements OnInit{
       this.triggerShake();
       setTimeout(() => {this.showAlert = false}, res.time);
     });
+
+  }
+
+  openModal(){
+    const modal = document.getElementById('staticBackdrop');
+    if (modal) {
+      this.renderer.removeClass(modal, 'modal-fade');
+      modal.setAttribute('aria-hidden', 'false');
+      modal.style.display = 'block';
+    }
+  }
+
+  closeModal() {
+    const modal = document.getElementById('staticBackdrop');
+    if (modal) {
+      this.renderer.addClass(modal, 'modal-fade');
+      modal.setAttribute('aria-hidden', 'true');
+      modal.style.display = 'none';
+    }
+  }
+
+  getProductLowStock() {
+    this.productService.getProductoLowStock().subscribe(
+      (products: ProductModel[]) => {
+        this.lowStockProducts = products;
+        console.log('Productos con bajo stock:', this.lowStockProducts);
+        if (this.lowStockProducts && this.lowStockProducts.length > 0) {
+          this.openModal();
+        }
+      },
+      error => {
+        console.error('Error al obtener productos con bajo stock:', error);
+      }
+    );
   }
 
   triggerShake() {
